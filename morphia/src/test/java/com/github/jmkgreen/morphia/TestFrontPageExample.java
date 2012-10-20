45b2b13e620d6ff21189410c52ff3f390536a588
James Green <james.mk.green@gmail.com>
2012-10-20 17:43:27 +0100

    Test to ensure getKey throws a MappingException on being called with a null value.

--- morphia/src/test/java/com/github/jmkgreen/morphia/TestFrontPageExample.java
+++ morphia/src/test/java/com/github/jmkgreen/morphia/TestFrontPageExample.java
@@ -32,6 +32,9 @@ import com.github.jmkgreen.morphia.annotations.NotSaved;
 import com.github.jmkgreen.morphia.annotations.Property;
 import com.github.jmkgreen.morphia.annotations.Reference;
 import com.github.jmkgreen.morphia.annotations.Transient;
+import com.github.jmkgreen.morphia.mapping.DefaultMapper;
+import com.github.jmkgreen.morphia.mapping.Mapper;
+import com.github.jmkgreen.morphia.mapping.MappingException;
 import com.github.jmkgreen.morphia.query.UpdateResults;
 
 /**
@@ -42,7 +45,7 @@ public class TestFrontPageExample  extends TestBase {
 
 	@Entity("employees")
 	private static class Employee {
-		
+
 		private Employee() {};
 		public Employee(String f, String l, Key<Employee> boss, long sal) {
 			firstName = f;
@@ -50,32 +53,32 @@ public class TestFrontPageExample  extends TestBase {
 			manager = boss;
 			salary = sal;
 		}
-		
+
 		@Id ObjectId id; // auto-generated, if not set (see ObjectId)
 		String firstName, lastName; // value types are automatically persisted
-		Long salary = null; // only non-null values are stored 
+		Long salary = null; // only non-null values are stored
 
 //		Address address; // by default fields are @Embedded
-		
+
 		Key<Employee> manager; // references can be saved without automatic
 								// loading
 		@Reference List<Employee> underlings = new ArrayList<Employee>(); // refs are stored*, and loaded automatically
-		
+
 		@Property("started") Date startDate; // fields can be renamed
 		@Property("left") Date endDate;
-		
+
 		@Indexed boolean active = false; // fields can be indexed for better performance
 		@NotSaved String readButNotStored; // fields can loaded, but not saved
 		@Transient int notStored; // fields can be ignored (no load/save)
 		transient boolean stored = true; // not @Transient, will be ignored by Serialization/GWT for example.
 	}
-	
+
 	@Test
     public void testIt() throws Exception {
 		morphia.map(Employee.class);
-		
+
 		ds.save(new Employee("Mister", "GOD", null, 0));
-		
+
 		Employee boss = ds.find(Employee.class).field("manager").equal(null).get(); // get an employee without a manager
 		Assert.assertNotNull(boss);
 		Key<Employee> scottsKey = ds.save(new Employee("Scott", "Hernandez", ds.getKey(boss), 150*1000));
@@ -85,7 +88,7 @@ public class TestFrontPageExample  extends TestBase {
 		Assert.assertNotNull(res);
 		Assert.assertTrue(res.getUpdatedExisting());
 		Assert.assertEquals(1, res.getUpdatedCount());
-		
+
 		Employee scottsBoss = ds.find(Employee.class).filter("underlings", scottsKey).get(); // get Scott's boss
 		Assert.assertNotNull(scottsBoss);
 		Assert.assertEquals(boss.id, scottsBoss.id);
@@ -93,6 +96,12 @@ public class TestFrontPageExample  extends TestBase {
 		for (Employee e : ds.find(Employee.class, "manager", boss))
 		   print(e);
 	}
-	
+
+    @Test(expected=MappingException.class)
+    public void getKeyWithNullThrowsException() throws MappingException {
+        Mapper mapper = new DefaultMapper();
+        Key<Employee> result = mapper.getKey(null);
+    }
+
 	private void print(Employee e) {};
 }
