9b9718498eadb87e74b38ca69a9dd3db76e8092b
James Green <james.mk.green@gmail.com>
2012-11-23 21:58:08 +0000

    Additional reflection tests.

--- morphia/src/test/java/com/github/jmkgreen/morphia/utils/ReflectionUtilsTest.java
+++ morphia/src/test/java/com/github/jmkgreen/morphia/utils/ReflectionUtilsTest.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package com.github.jmkgreen.morphia.utils;
 
@@ -14,9 +14,11 @@ import junit.framework.Assert;
 import org.junit.Test;
 
 import com.github.jmkgreen.morphia.TestBase;
+import com.github.jmkgreen.morphia.annotations.Entity;
 import com.github.jmkgreen.morphia.annotations.Id;
 import com.github.jmkgreen.morphia.annotations.Index;
 import com.github.jmkgreen.morphia.annotations.Indexes;
+import com.github.jmkgreen.morphia.mapping.Mapper;
 
 /**
  * @author Uwe Schaefer, (us@thomas-daily.de)
@@ -36,21 +38,34 @@ public class ReflectionUtilsTest extends TestBase
         Assert.assertTrue(ReflectionUtils.implementsInterface(ArrayList.class, Collection.class));
         Assert.assertFalse(ReflectionUtils.implementsInterface(Set.class, List.class));
     }
-    
+
     @Test
     public void testInheritedClassAnnotations() {
     	List<Indexes> annotations = ReflectionUtils.getAnnotations(Foobie.class, Indexes.class);
     	Assert.assertEquals(2, annotations.size());
     	Assert.assertTrue(ReflectionUtils.getAnnotation(Foobie.class, Indexes.class) instanceof Indexes);
+
+        Entity foobie = ReflectionUtils.getClassEntityAnnotation(Foobie.class);
+        Assert.assertTrue("Sub".equals(foobie.value()));
+
+        Entity fooble = ReflectionUtils.getClassEntityAnnotation(Fooble.class);
+        Assert.assertEquals(Mapper.IGNORED_FIELDNAME, fooble.value());
     }
 
+    @Entity("Base")
     @Indexes(@Index("id"))
     private static class Foo {
     	@Id int id;
     }
-    
+
+    @Entity("Sub")
     @Indexes(@Index("test"))
     private static class Foobie extends Foo {
     	String test;
+    }
+
+    @Entity()
+    private static class Fooble extends Foobie {
+
     }
 }
